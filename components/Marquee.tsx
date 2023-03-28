45bdb0db37f243eaee7b81f0281a75036cba0885
Martín Morici <martinmorici3@gmail.com>
2023-03-27 21:06:21 -0300

    cambios finales

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -8,7 +8,7 @@ const Marquee = () => {
   return (
     <div className='relative'>
     <Image className='hidden md:block absolute left-[7vw] mt-[-12px] z-30' src={Marqueeizq} alt='Pincho Marquee' />
-    <div className='border-y-[1.5px] mt-[60px]  flex text-[24px] sm:text-[35px] font-bold py-[14px] overflow-x-hidden '>
+    <div className='border-y-[1.5px] mt-3 md:mt-[60px]  flex text-[16px] md:text-[24px] sm:text-[35px] font-bold  py-2 md:py-[14px] overflow-x-hidden '>
       <div className='animate-marquee whitespace-nowrap  '> 
         <span>A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag —  </span>
         <span>A man can’t have enough base­ment swag  —  A man can’t have enough base­ment swag — </span>
